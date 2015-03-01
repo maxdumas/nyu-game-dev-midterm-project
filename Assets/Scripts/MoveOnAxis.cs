b88b2a31f7928794b2c922f0e83dd0ebcf8bb3e2
Max Dumas <maltor124@gmail.com>
2015-02-28 19:14:19 -0500

    Add working lasers, round controller, text timer.

---
+++ Assets/Scripts/MoveOnAxis.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnAxis : MonoBehaviour {
+	public float Speed;
+	public float Duration;
+
+	private float _t;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_t < Duration) {
+			transform.position += Time.deltaTime * Speed * transform.up;
+			_t += Time.deltaTime;
+		} else {
+			this.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/RoundController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RoundController : MonoBehaviour {
+	public int Round;
+	public GameObject[] LaserRounds;
+	public float RoundWarmupTime;
+	public Text display;
+	public float SloMoTimeScale;
+
+	private float _t;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ManageRounds());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_t < RoundWarmupTime) {
+			display.text = Mathf.Floor(RoundWarmupTime - _t + 1f).ToString();
+			_t += Time.deltaTime;
+			Time.timeScale = Mathf.SmoothStep(Time.timeScale, 1f, 0.125f);
+		} else {
+			display.text = "GO!!!";
+			Time.timeScale = Mathf.SmoothStep(Time.timeScale, SloMoTimeScale, 0.125f);
+		}
+
+		Time.fixedDeltaTime = 0.02f * Time.timeScale;
+	}
+
+	IEnumerator ManageRounds() {
+		foreach(GameObject round in LaserRounds) {
+			++Round;
+			_t = 0f;
+			round.SetActive(true);
+			yield return new WaitForSeconds(RoundWarmupTime);
+			float longestWaitTime = 0f;
+			foreach(Transform child in round.transform) {
+				var c = child.gameObject.GetComponent<MoveOnAxis>();
+				c.enabled = true;
+				longestWaitTime = Mathf.Max (longestWaitTime, c.Duration);
+			}
+			yield return new WaitForSeconds(longestWaitTime);
+		}
+	}
+}
